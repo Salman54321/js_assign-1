aa43d4dcc82d5491ba9a856f21ee7dc44ae073bd
Salman54321 <50177174+Salman54321@users.noreply.github.com>
2023-03-30 22:12:14 +0500

    Add files via upload

---
+++ js_assign-1.js
@@ -0,0 +1,63 @@
+// Question 1
+var age = 25;
+/* variable defined wit let can not be redeclared */
+let age_as_string = "age";
+console.log(age);
+console.log(age_as_string);
+
+
+// Question 2
+let number = prompt("Enter a Number to check either even or odd");
+if (number % 2 == 0)
+{
+  alert(number + 'Number is even');
+}
+else 
+{
+  alert(number + 'Number is odd')
+}
+
+// Question 3
+let temp;
+temp = 72;
+console.log(temp+' '+ 'Temperature in Fahrenheit');
+let celsius = (temp - 32) * 5/9;
+console.log(celsius +' '+ 'Temperature in celsius');
+
+// Question 4
+let name = prompt('enter your name')
+let age = prompt('enter you age')
+
+/* if the age is greater than and equals to 18 */
+if (age >= 18)
+{
+  console.log('Welcome ${name}, you are an adult');
+}
+else
+{
+  console.log('Welcome ${name}, you are not adult')
+}
+
+// Question 5
+let is_Raining =! false;
+
+if (is_Raining)
+{
+  console.log('Bring an Umbrella');
+}
+else 
+{
+  console.log('Leave Umbrella at Home');
+}
+
+/* another way */
+let isRaining = true;
+
+if (isRaining)
+{
+  console.log('Bring an Umbrella');
+}
+else 
+{
+  console.log('Leave Umbrella at Home');
+}
